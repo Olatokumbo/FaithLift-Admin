cfbfac2e40c1b7bbaff26bb297bff8a15a221761
David Odesola <faithodesola@gmail.com>
2021-02-25 11:31:41 +0100

    Constructing the Movies Table Layout

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,16 +1,70 @@
-import React from "react";
-import { Typography, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Typography,
+  IconButton,
+  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+  Paper,
+} from "@material-ui/core";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import style from "./Movies.module.css";
 
 const Movies = () => {
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+
   return (
     <div className={style.moviesPage}>
       <Typography className={style.title}>Movies</Typography>
-      <Button className={style.addBtn} variant="contained" color="primary">Add Movie</Button>
-      <div className={style.t}></div>
+      <Button className={style.addBtn} variant="contained" color="primary">
+        Add Movie
+      </Button>
+      <TableContainer className={style.table} component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="center">Writer</TableCell>
+              <TableCell align="center">Director</TableCell>
+              <TableCell align="center">Year Released</TableCell>
+              <TableCell align="center">View</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.fat}</TableCell>
+                <TableCell align="center">{row.carbs}</TableCell>
+                <TableCell align="center">{row.protein}</TableCell>
+                <TableCell align="center">
+                  <IconButton>
+                    <VisibilityIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
